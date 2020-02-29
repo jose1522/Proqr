2125b9e84dc5b06db4cc6e689be384dc2ce6464a
Jesram1001 <44485002+Jesram1001@users.noreply.github.com>
2020-02-28 20:13:16 -0600

    Adding SendGrid (sent email/password recovery)

--- app/objects/Integration/DB/addUser.py
+++ app/objects/Integration/DB/addUser.py
@@ -1,4 +1,5 @@
 import requests
+from app.objects.sendGrid import SendEmail
 from app.objects.Integration.DB.readKey import readDBKey
 from app.objects.newPassword import CreatePassword
 
@@ -6,16 +7,17 @@ def AddUser(user):
     key = readDBKey()
 
     url = "https://jskr4ovkybl0gsf-db202002091757.adb.us-ashburn-1.oraclecloudapps.com/ords/tables/api/users"
-
+    password = CreatePassword()
     headers = {
         'X-NAME': user.firstName,
         'X-LAST': user.lastName,
         'X-EMAIL': user.email,
-        'X-PASSWORD': CreatePassword(),
+        'X-PASSWORD': password,
         'X-ROLE': str(user.role),
 
         'Authorization': "Basic {0}".format(key)
     }
 
     response = requests.request("POST", url, headers=headers)
+    SendEmail(sender='noreply@email.com', to=user.email, subject='Your New Password',body=password)
     return response.status_code
